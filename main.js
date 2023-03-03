5fef2b456e2b1ee2d53689e190738d044b2e7d15
unknown <kevinnoriega2014@gmail.com>
2023-03-02 20:19:49 -0500

    segundo commit

--- main.js
+++ main.js
@@ -5,27 +5,33 @@ function calcular() {
     let estrato = document.getElementById("estrato").value;
     let metros = document.getElementById("metros").value;
     
-    let estrato1= (250*metros)+2300*0.60
-    let estrato2= (350*metros)+3200*0.70
-    let estrato3= (460*metros)+3900*0.90
 
 
     if(estrato==1){
-         total= estrato1
+         total= (250*metros)+2300*0.60
+         cargoFijo=2300
+         valorSubsidio=2300*0.40
+         porcentajeSubsidio=40
     }else if(estrato==2){
-         total=estrato2
+         total=(350*metros)+3200*0.70
+         valorSubsidio=3200*0.30
+         cargoFijo=3200
+         porcentajeSubsidio=30
     }else {
-         total=estrato3
+         total=(460*metros)+3900*0.90
+         valorSubsidio=3900*0.10
+         cargoFijo=3900
+         porcentajeSubsidio=10
     }
-    setLocalStorage(usuario, estrato, metros,total);
+    setLocalStorage(usuario, estrato, metros,total,valorSubsidio,cargoFijo,porcentajeSubsidio);
 
 }
 let arrayDatosAguasan = JSON.parse(localStorage.getItem('arrayDatosAguasan')) || [];
 
-function setLocalStorage(usuario, estrato, metros,total) {
+function setLocalStorage(usuario, estrato, metros,total,valorSubsidio,cargoFijo,porcentajeSubsidio) {
     
     
-    arrayDatosAguasan.push({ usuario: usuario, estrato: estrato, metros: metros, total:total })
+    arrayDatosAguasan.push({ usuario: usuario, estrato: estrato,cargoFijo:cargoFijo, metros: metros, porcentajeSubsidio:porcentajeSubsidio, valorSubsidio:valorSubsidio,total:total  })
     localStorage.setItem('arrayDatosAguasan', JSON.stringify(arrayDatosAguasan))
   
 
